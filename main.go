54968fcbe77e85d6e49a242b53740c16e0f18267
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-02-12 13:02:30 -0600

    Use ServeConn method to handle h2c connection.

--- main.go
+++ main.go
@@ -8,12 +8,29 @@ import (
 	"os"
 
 	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 )
 
 const (
 	unixSocketPath = "./socket"
 )
+
+func runConnectionHandler(
+	conn net.Conn,
+	handler http.Handler,
+	http2Server *http2.Server,
+) {
+	log.Printf("begin runConnectionHandler")
+
+	defer conn.Close()
+
+	http2Server.ServeConn(
+		conn,
+		&http2.ServeConnOpts{
+			Handler: handler,
+		})
+
+	log.Printf("end runConnectionHandler")
+}
 
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
@@ -27,22 +44,19 @@ func main() {
 		log.Fatalf("net.Listen error path = %v: %v", unixSocketPath, err)
 	}
 
-	h2s := &http2.Server{
-		// ...
-	}
+	http2Server := &http2.Server{}
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("got http handler call r = %+v", r)
 		fmt.Fprint(w, "Hello world")
 	})
 
-	h1s := &http.Server{
-		Handler: h2c.NewHandler(handler, h2s),
+	for {
+		conn, err := netListener.Accept()
+		if err != nil {
+			log.Fatalf("Accept error: %v", err)
+		}
+
+		go runConnectionHandler(conn, handler, http2Server)
 	}
-
-	log.Printf("before h1s.Serve")
-
-	err = h1s.Serve(netListener)
-
-	log.Fatalf("h1s.Serve error: %v", err)
 }
